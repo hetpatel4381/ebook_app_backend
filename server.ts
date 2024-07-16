35e933899be33c6dd4ed4c33e70584d06f3a03f3
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-16 16:02:57 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome to Ebook apis");
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
+
+startServer();
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+// Routes
+// Http: methods:- GET, POST, PATCH, DELETE.
+app.get("/", (req, res, next) => {
+  res.json({ message: "Welcome to elib apis" });
+});
+
+export default app;
