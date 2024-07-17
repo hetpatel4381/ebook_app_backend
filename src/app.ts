7021706de9d6b9956e5833f1d6dcf5a49153e1f4
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-17 05:30:50 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRoutes from "./user/userRoutes";
 
 const app = express();
 
@@ -8,6 +9,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to elib apis" });
 });
+
+app.use("/api/users", userRoutes);
 
 // Global error handler.
 app.use(globalErrorHandler);
