1b12e288d458dda873450c7cc4924f322a31f5b2
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-17 06:23:29 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -22,9 +24,21 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // password -> hash
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  // create new user
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // Token generation JWT
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
   // process
   // response
-  return res.json({ message: "User Created!" });
+  return res.json({ accessToken: token });
 };
 
 export { createUser };
