e28427a3c7b49d5b996232d168b81e4ce262555d
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-07-20 19:34:50 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRoutes from "./user/userRoutes";
 import bookRoutes from "./book/bookRoutes";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 
 // Routes
